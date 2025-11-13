ecbad6c36e3ae14ed856875f550f228db8f046e0
unknown <omurkanovd22@gmail.com>
2025-11-13 13:13:35 +0600

    images

--- product/models.py
+++ product/models.py
@@ -9,11 +9,19 @@ class Product(models.Model):
     article = models.IntegerField(verbose_name="Артикул", null=True)
     discount = models.PositiveIntegerField(verbose_name="Процент скидки", null=True)
     price_with_discount = models.DecimalField(verbose_name="Цена со скидкой", max_digits=10, decimal_places=2, null=True, blank=True)
-
+    amount = models.PositiveIntegerField(verbose_name="количество", default=0)
+ 
     def __str__(self):
         return self.name
     
     def save(self, *args, **kwargs):
         self.price_with_discount = self.price * (1 - Decimal(self.discount) / Decimal(100))
         super().save(*args, **kwargs)
+
+
+class ProductImage(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True)
+    image = models.ImageField("фотография", upload_to="product")
+
+    
         
